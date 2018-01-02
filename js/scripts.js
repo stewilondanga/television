93745ec763a4e29f63c9da8b86d4009ae6558af3
Stewart ilondanga <stilondanga@gmail.com>
2018-01-02 10:08:13 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,76 @@ function onYouTubeIframeAPIReady() {
 				function onReady(event) {
 		         var player = event.target;
 
- 
+						 player.loadVideoById({suggestedQuality: suggestedQuality,
+						                              videoId: videoId
+						                             });
+						        player.pauseVideo();
+						        youTubePlayerDisplayFixedInfos();
+						    }
+
+								function onStateChange(event) {
+						         var volume = Math.round(event.target.getVolume());
+						         var volumeItem = document.getElementById(youTubePlayerVolumeItemId);
+
+										 if (volumeItem && (Math.round(volumeItem.value) != volume)) {
+							            volumeItem.value = volume;
+							        }
+							    }
+
+									youTubePlayer = new YT.Player('YouTube-player',
+							                                  {videoId: videoId,
+							                                   height: height,
+							                                   width: width,
+							                                   playerVars: {'autohide': 0,
+							                                                'cc_load_policy': 0,
+							                                                'controls': 2,
+							                                                'disablekb': 1,
+							                                                'iv_load_policy': 3,
+							                                                'modestbranding': 1,
+							                                                'rel': 0,
+							                                                'showinfo': 0,
+							                                                'start': 3
+							                                               },
+							                                   events: {'onError': onError,
+							                                            'onReady': onReady,
+							                                            'onStateChange': onStateChange
+							                                           }
+							                                  });
+
+							    // Add private data to the YouTube object
+							    youTubePlayer.personalPlayer = {'currentTimeSliding': false,
+							                                    'errors': []};
+							}
+
+
+							/**
+							 * :return: true if the player is active, else false
+							 */
+							function youTubePlayerActive() {
+							    'use strict';
+
+							    return youTubePlayer && youTubePlayer.hasOwnProperty('getPlayerState');
+							}
+
+
+							/**
+							 * Get videoId from the #YouTube-video-id HTML item value,
+							 * load this video, pause it
+							 * and show new infos.
+							 */
+							function youTubePlayerChangeVideoId() {
+							    'use strict';
+
+							    var inputVideoId = document.getElementById('YouTube-video-id');
+							    var videoId = inputVideoId.value;
+
+							    youTubePlayer.cueVideoById({suggestedQuality: 'tiny',
+							                                videoId: videoId
+							                               });
+							    youTubePlayer.pauseVideo();
+							    youTubePlayerDisplayFixedInfos();
+							}
+
 
 var navigate = (function() {
 	$('.dd').toggle();
